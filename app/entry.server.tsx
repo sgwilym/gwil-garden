e3bd3103f7bccc008d3afbee8e64f085d699d58d
Sam Gwilym <gwilym@me.com>
2021-06-13 16:12:04 +0000

    Add limited pub functionality to server entry

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,13 +1,81 @@
 import ReactDOMServer from "react-dom/server";
 import type { EntryContext } from "remix";
-import { RemixServer } from "remix";
+import { RemixServer, json } from "remix";
+import dotenv from "dotenv";
+import { getGardenStorage } from "./workspace/storage.server";
+import { Document, WriteResult } from "earthstar";
 
-export default function handleRequest(
+export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
+  dotenv.config();
+
+  // TODO: RSS.
+  /*
+  if (new URL(request.url).pathname === "/rss.xml") {
+    return new Response(await rss(), {
+      headers: {
+        "Content-Type": "application/xml",
+      },
+    });
+  }
+  */
+
+  // https://github.com/earthstar-project/earthstar-pub/blob/master/src/pub.ts
+
+  const pathname = new URL(request.url).pathname;
+  const API_PREFIX = `/earthstar-api/v1/${process.env.GARDEN_WORKSPACE}`;
+
+  if (pathname.startsWith(API_PREFIX)) {
+    const storage = getGardenStorage();
+
+    if (!storage) {
+      return new Response(undefined, {
+        status: 404,
+      });
+    }
+
+    // list paths
+    // /earthstar-api/v1/:workspace/paths
+    if (pathname === `${API_PREFIX}/paths` && request.method === "GET") {
+      return json(storage.paths());
+    }
+
+    // get all documents
+    // /earthstar-api/v1/:workspace/documents
+    if (pathname === `${API_PREFIX}/documents` && request.method === "GET") {
+      return json(storage.documents({ history: "all" }));
+    }
+
+    // ingest documents (uploaded from client)
+    // /earthstar-api/v1/:workspace/documents
+    if (pathname === `${API_PREFIX}/documents` && request.method === "POST") {
+      const docs: Document[] = await request.json();
+
+      if (!Array.isArray(docs)) {
+        return new Response(undefined, { status: 404 });
+      }
+
+      let numIngested = 0;
+      for (let doc of docs) {
+        if (storage.ingestDocument(doc, "🙂") === WriteResult.Accepted) {
+          numIngested += 1;
+        }
+      }
+
+      return json({
+        numIngested: numIngested,
+        numIgnored: docs.length - numIngested,
+        numTotal: docs.length,
+      });
+    }
+
+    // Don't do live streaming.
+  }
+
   let markup = ReactDOMServer.renderToString(
     <RemixServer context={remixContext} url={request.url} />
   );
@@ -16,7 +84,7 @@ export default function handleRequest(
     status: responseStatusCode,
     headers: {
       ...Object.fromEntries(responseHeaders),
-      "Content-Type": "text/html"
-    }
+      "Content-Type": "text/html",
+    },
   });
 }
